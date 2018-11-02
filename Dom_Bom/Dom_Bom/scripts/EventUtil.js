82c6dabca9284e02f62a7f8a8ce1b71646002677
kenSoldier <gnkensoldier@gmail.com>
2018-11-03 01:15:51 +0800

    add file

---
+++ Dom_Bom/Dom_Bom/scripts/EventUtil.js
@@ -0,0 +1,54 @@
+﻿(function (window) {
+    //封裝一個綁定事件的兼容處理
+    var EventUtil = {
+    //1.給誰綁定 2.綁定什麼事件 3. 綁定事件的觸裡程序  4.捕獲冒泡?
+    //要寫成  EventUtil.addEvent(element, 'click', function (e) { },true/false)
+        addEvent:function (element,type,handler,isCapture) {
+            if (element.addEventListener) {
+                //判斷是否冒泡
+                isCapture = isCapture ? true : false;
+                element.addEventListener(type, handler, isCapture);
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + type, function () {
+                    //要把e傳進去 是因為ie的e=window.event,而其他不用
+                    return handler.call(element, window.event);
+                    //如果是一般函數調用模式this= window 而不是event.target||event.srcElement 所以借用call
+                })
+            }
+        },
+
+        //獲得事件
+        getEvent: function (e) {
+            return e || window.event;
+        },
+
+        //獲得事件原對象
+        getTarget: function (e) {
+            return e.target || e.srcElement;
+        },
+
+        //阻止冒泡行為
+        stopPropagation: function (e) {
+            if (e.stopPropagation) {
+                e.stopPropagation()
+            } else {
+                e.cancleBubble = true;
+            }
+        },
+
+        //阻止默認行為
+        preventDefault: function (e) {
+            if (e.preventDefault) {
+                e.preventDefault();
+            } else {
+                e.returnValue = false;
+            }
+
+        }
+
+    };
+    //把上面所對應的對象賦值給window的屬性 
+    window.EventUtil = EventUtil;
+    //傳window 讓window接收
+})(window || {})
+
---
+++ Dom_Bom/Dom_Bom/scripts/documentReady.js
@@ -0,0 +1,18 @@
+﻿//Jquery 實現文檔加載
+/*
+       * jquery實現文檔加載程序處理
+       * @callback function 頁面加載完執行回掉函數       
+      */
+document.myReady = function (callback) {
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', callback, false);
+    } else if (document.attachEvent) {    //兼容IE8以下的瀏覽
+        document.attachEvent('onreadystatechange', function () {
+            if (document.readyState == 'interactive') {
+                callback(window.event);
+            }
+        })
+    } else {//其他特殊情況
+        window.onload = callback;
+    }
+}
